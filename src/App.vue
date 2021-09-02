10d7db201732292ea0660f9cfc4c461bde0a1c98
zzl <188877869@qq.com>
2021-09-02 17:18:01 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Header></Header>
+</template>
+
+<script  lang="ts">
+import { defineComponent } from 'vue'
+import Header from '@/components/Header/index.vue'
+
+export default defineComponent({
+  components:{
+    Header,
+  },
+  setup(){},
+})
+
+</script>
