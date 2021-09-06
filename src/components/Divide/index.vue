0ae1aafe269bb428a6a06629ce952ebff0d751cd
zzl1994 <zhanzili1994@163.com>
2021-09-06 21:24:55 +0800

    divide line style update

--- src/components/Divide/index.vue
+++ src/components/Divide/index.vue
@@ -6,39 +6,39 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { Icon } from '@iconify/vue'
+  import { defineComponent } from 'vue'
+  import { Icon } from '@iconify/vue'
 
-export default defineComponent({
-  components: {
-    Icon
-  },
-  setup() {}
-})
+  export default defineComponent({
+    components: {
+      Icon
+    },
+    setup() {}
+  })
 </script>
 
 <style scoped>
-.divide {
-  @apply text-blue-400 font-semibold pt-4 pb-4 items-center relative cursor-pointer;
+  .divide {
+    @apply text-blue-300 font-semibold pt-5 pb-5 items-center relative cursor-pointer;
 
-  font-size: 20px;
-}
+    font-size: 22px;
+  }
 
-.divide hr {
-  @apply border-2 border-dashed border-blue-400;
+  .divide hr {
+    @apply border-2 border-dashed border-blue-300;
 
-  margin: 0 auto;
-}
+    margin: 0 auto;
+  }
 
-.divide .divide-icon {
-  z-index: 1;
-  position: absolute;
-  top: 8px;
-  left: 10px;
-  transition: all 1s ease-in-out;
-}
+  .divide .divide-icon {
+    z-index: 1;
+    position: absolute;
+    top: 11px;
+    left: 14px;
+    transition: all 1s ease-in-out;
+  }
 
-.divide:hover .divide-icon {
-  left: calc(100% - 27px);
-}
+  .divide:hover .divide-icon {
+    left: calc(100% - 30px);
+  }
 </style>
