795caa5b279e69c20aa2cdd299a8e50f69d8060c
zzl1994 <zhanzili1994@163.com>
2021-09-06 22:23:16 +0800

    add sidebar menu

--- src/layouts/Header/components/Menu.vue
+++ src/layouts/Header/components/Menu.vue
@@ -15,55 +15,55 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watch } from 'vue'
-import { Icon } from '@iconify/vue'
-import { useRoute } from 'vue-router'
-import { routes } from '@/routes'
+  import { defineComponent, ref, watch } from 'vue'
+  import { Icon } from '@iconify/vue'
+  import { useRoute } from 'vue-router'
+  import { routes } from '@/routes'
 
-export default defineComponent({
-  components: {
-    Icon
-  },
-  setup() {
-    const route = useRoute()
-    const path = ref(route.path)
-    watch(
-      () => route.path,
-      (p) => (path.value = p)
-    )
-    const menus = routes[0].children
-    return { path, menus }
-  }
-})
+  export default defineComponent({
+    components: {
+      Icon
+    },
+    setup() {
+      const route = useRoute()
+      const path = ref(route.path)
+      watch(
+        () => route.path,
+        (p) => (path.value = p)
+      )
+      const menus = routes[0].children
+      return { path, menus }
+    }
+  })
 </script>
 
 <style scoped>
-.menu {
-  @apply flex items-center;
-}
+  .menu {
+    @apply flex items-center;
+  }
 
-.menu .menu-item {
-  @apply pl-4 pr-4 h-16 box-border font-medium;
-  @apply text-gray-700 flex items-center space-x-1 flex-shrink-0;
+  .menu .menu-item {
+    @apply pl-4 pr-4 h-16 box-border font-medium;
+    @apply text-gray-700 flex items-center space-x-1 flex-shrink-0;
 
-  border-bottom: 3px solid transparent;
-}
+    border-bottom: 3px solid transparent;
+  }
 
-.menu .menu-item:hover {
-  @apply bg-gray-200;
-}
+  .menu .menu-item:hover {
+    @apply bg-gray-200;
+  }
 
-.menu .menu-item span {
-  @apply text-gray-700 font-medium text-sm;
-}
+  .menu .menu-item span {
+    @apply text-gray-700 font-medium text-sm;
+  }
 
-.menu .active {
-  @apply text-blue-500;
+  .menu .active {
+    @apply text-blue-500;
 
-  border-bottom: 3px solid #3b82f6;
-}
+    border-bottom: 3px solid #3b82f6;
+  }
 
-.menu .active span {
-  @apply text-blue-500;
-}
+  .menu .active span {
+    @apply text-blue-500;
+  }
 </style>
--- src/layouts/Sidebar/components/Menus.vue
+++ src/layouts/Sidebar/components/Menus.vue
@@ -1,8 +1,44 @@
-<template> </template>
+<template>
+  <div class="sitebar-menus">
+    <router-link v-for="(menu, index) in menus" :key="index" :to="menu.path" class="sitebar-menu">
+      <span class="w-6">
+        <Icon :icon="menu.meta.icon" />
+      </span>
+      <span>{{ menu.meta.title }}</span>
+    </router-link>
+  </div>
+</template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+  import { defineComponent } from 'vue'
+  import { routes } from '@/routes'
+  import { Icon } from '@iconify/vue'
 
-export default defineComponent({
-  setup() {}
-})
+  export default defineComponent({
+    components: {
+      Icon
+    },
+    setup() {
+      const menus = routes[0].children
+      return { menus }
+    }
+  })
 </script>
+
+<style scoped>
+  .sitebar-menus {
+    @apply space-y-4 mt-6;
+  }
+
+  .sitebar-menus .sitebar-menu {
+    @apply flex items-center  space-x-8;
+    @apply pl-4 pr-4 pt-2 pb-2 ml-4 mr-4 cursor-pointer;
+  }
+
+  .sitebar-menus .sitebar-menu:hover {
+    @apply bg-blue-300;
+  }
+
+  .sitebar-menus .sitebar-menu span {
+    @apply text-lg font-medium text-gray-700;
+  }
+</style>
